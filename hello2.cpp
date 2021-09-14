85d9c873ac3552e64725079d9f138d0d8a57a4cd
jm2601 <64814944+jm2601@users.noreply.github.com>
2021-09-13 20:12:26 -0700

    Lab 1

---
+++ hello2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string myName;
+    cout << "Please enter your name: ";
+    cin >> myName;
+    cout << "Welcome to CSE030, " << myName << "!" << endl;
+
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Welcome to CSE 030!" << endl;
+    return 0;
+}
